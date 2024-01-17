679c3b573ecb36a26b41a826882bc276833cc7b0
Irina Tomic <irina.z.tomic@gmail.com>
2024-01-17 18:02:12 +0100

    Init auth_servis

---
+++ auth_servis/app/models/korisnik.rb
@@ -0,0 +1,13 @@
+# app/models/korisnik.rb
+
+class Korisnik < ApplicationRecord
+    self.table_name = 'Korisnik'
+    self.record_timestamps = false
+
+    has_many :narudzbine, class_name: 'Narudzbina', foreign_key: 'korisnik_id', dependent: :destroy
+  
+    validates :username, presence: true, uniqueness: true
+    validates :password, presence: true
+    validates :admin, inclusion: { in: [true, false] }
+    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Nije ispravna email adresa" }
+end
